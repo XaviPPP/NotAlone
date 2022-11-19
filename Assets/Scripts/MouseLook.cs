aecc19f5f5505a0bc9a8efb5839c71e96a70ce27
XaviPPP <46970312+XaviPPP@users.noreply.github.com>
2022-11-19 16:30:40 +0000

    Clipping da camera prevenido

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Jobs;
 
 public class MouseLook : MonoBehaviour
 {
-
     public float mouseSens = 100f;
 
     public Transform playerBody;
@@ -31,7 +31,7 @@ public class MouseLook : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
 
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, -85f, 85f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
