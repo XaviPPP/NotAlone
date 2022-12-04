e02739a6a09e39a7efd0196483bc66e01ef68a01
warzone520PT <warzoney@hotmail.com>
2022-12-04 17:33:48 +0000

    Porta que abre (n sei pq a camara esta estranha)

---
+++ Assets/Scripts/DoorWood.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorWood : MonoBehaviour
+{
+    [SerializeField] private Animator door;
+    [SerializeField] private GameObject openDoorButton;
+    [SerializeField] private AudioSource doorAudioSource;
+    [SerializeField] private AudioClip[] doorOpenClips;
+    [SerializeField] private AudioClip[] doorCloseClips;
+    [SerializeField] private AudioClip[] doorLockedClips;
+    [SerializeField] private bool isLocked;
+    private bool isOpen;
+    private bool inReach;
+
+    void Start()
+    {
+        inReach = false;
+        isOpen = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            openDoorButton.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            openDoorButton.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (inReach && Input.GetButtonDown("Interact") && isLocked)
+        {
+            doorAudioSource.PlayOneShot(doorLockedClips[UnityEngine.Random.Range(0, doorLockedClips.Length)]);
+        }
+        else if (inReach && Input.GetButtonDown("Interact") && !isOpen)
+        {
+            DoorOpens();
+            isOpen = true;
+        }
+        else if(inReach && Input.GetButtonDown("Interact") && isOpen)
+        {
+            DoorCloses();
+            isOpen = false;
+        }
+    }
+
+    private void DoorOpens()
+    {
+        door.SetBool("Open", true);
+        door.SetBool("Closed", false);
+        doorAudioSource.PlayOneShot(doorOpenClips[UnityEngine.Random.Range(0, doorOpenClips.Length)]);
+    }
+
+    private void DoorCloses()
+    {
+        door.SetBool("Open", false);
+        door.SetBool("Closed", true);
+        doorAudioSource.PlayOneShot(doorCloseClips[UnityEngine.Random.Range(0, doorCloseClips.Length)]);
+    }
+}
