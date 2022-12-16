d46356788bd15921d6d0466855ed0641794574de
warzone520PT <warzoney@hotmail.com>
2022-12-16 22:29:37 +0000

    HorrorFPS14

---
+++ Assets/Horror FPS KIT/HFPS Assets/Content/Scripts/Editor/Components/WeaponControllerEditor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using HFPS.Player;
+
+namespace HFPS.Editors
+{
+    [CustomEditor(typeof(WeaponController)), CanEditMultipleObjects]
+    public class WeaponControllerEditor : Editor
+    {
+        SerializedProperty m_shotgunSettings;
+        SerializedProperty m_bulletModelSettings;
+
+        WeaponController controller;
+
+        void OnEnable()
+        {
+            controller = target as WeaponController;
+            m_shotgunSettings = serializedObject.FindProperty("shotgunSettings");
+            m_bulletModelSettings = serializedObject.FindProperty("bulletModelSettings");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.UpdateIfRequiredOrScript();
+
+            DrawPropertiesExcluding(serializedObject, "shotgunSettings", "bulletModelSettings");
+
+            if (controller.bulletType == WeaponController.BulletType.Bullet)
+            {
+                EditorGUILayout.PropertyField(m_bulletModelSettings, true);
+            }
+            if (controller.weaponType == WeaponController.WeaponType.Shotgun)
+            {
+                EditorGUILayout.PropertyField(m_shotgunSettings, true);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
