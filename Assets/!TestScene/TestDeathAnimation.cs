26b8a0aad85e68752ed9ce2176452fead237818b
XaviPPP <46970312+XaviPPP@users.noreply.github.com>
2022-12-18 22:27:58 +0000

    Animação de morte adicionada (carregar no E)

---
+++ Assets/!TestScene/TestDeathAnimation.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDeathAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            animator.SetBool("isDead", true);
+        }
+    }
+}
